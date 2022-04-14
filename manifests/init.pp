70a1b82f2b7757d1020722b61fa6e054061e2412
Martez Reed <martez.reed@greenreedtech.com>
2022-04-14 14:01:29 -0500

    Initial code commit

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class nginx (
+  String  $version           = '1.18.0-0ubuntu1.2',
+  String  $secretdata        = 'not secret',
+)
+{
+  package { 'nginx':
+    ensure => $version,
+  }
+
+  $os_details = $facts['os']['distro']['description']
+
+  file {'/var/www/html/index.html':
+    ensure  => file,
+    path    => '/var/www/html/index.html',
+    mode    => '0644',
+    content => epp('nginx/index.html.epp', {'os_details' => $os_details,'secretdata' => $secretdata}),
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => 'running',
+    name   => 'nginx',
+    enable => true,
+  }
+}
